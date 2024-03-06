838dd839ca1d8abbf7408f206f50d33174a0a968
CHOUICH <nacer-eddine.chouich@capgemini.com>
2024-03-06 12:31:52 +0100

    Home + about done

--- client/app/components/Home.jsx
+++ client/app/components/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
               </div>
             </Link>
           </div>
-          <div className="absolute lg:left-auto left-[10%] lg:bottom-10 bottom-[100%] flex lg:justify-start gap-6 lg:items-center">
+          <div className="absolute lg:left-auto left-[20%] lg:bottom-10 bottom-[100%] flex lg:justify-start gap-6 lg:items-center">
             <Link
               href="https://www.instagram.com/nacer_edd/?igsh=ZHY2bnB1d2puMWMy&utm_source=qr"
               target="_blanc"
