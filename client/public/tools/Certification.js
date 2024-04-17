2f3b0401e0c53ca1ceb696048a394ba5261f54b1
CHOUICH <nacer-eddine.chouich@capgemini.com>
2024-04-17 15:10:44 +0100

    Added IBM Full Stack Software Developer Professional Certificate

--- client/public/tools/Certification.js
+++ client/public/tools/Certification.js
@@ -3,30 +3,30 @@ const Certifications = [
     title:"IBM Full Stack Software Developer Professional Certificate",
     from:"IBM"
   },
-  {
-    title: "Introduction to Cloud Computing",
-    from: "IBM",
-  },
-  {
-    title: "Introduction to Web Development with HTML, CSS, JavaScript",
-    from: "IBM",
-  },
-  {
-    title: "Getting Started with Git and GitHub",
-    from: "IBM",
-  },
-  {
-    title: "Developing Front-End Apps with React",
-    from: "IBM",
-  },
-  {
-    title: "Developing Back-End Apps with Node.js and Express",
-    from: "IBM",
-  },
-  {
-    title: "Python for Data Science, AI & Development",
-    from: "IBM",
-  },
+  // {
+  //   title: "Introduction to Cloud Computing",
+  //   from: "IBM",
+  // },
+  // {
+  //   title: "Introduction to Web Development with HTML, CSS, JavaScript",
+  //   from: "IBM",
+  // },
+  // {
+  //   title: "Getting Started with Git and GitHub",
+  //   from: "IBM",
+  // },
+  // {
+  //   title: "Developing Front-End Apps with React",
+  //   from: "IBM",
+  // },
+  // {
+  //   title: "Developing Back-End Apps with Node.js and Express",
+  //   from: "IBM",
+  // },
+  // {
+  //   title: "Python for Data Science, AI & Development",
+  //   from: "IBM",
+  // },
   {
     title: "Fundamentals of Java Programming",
     from: "BooRD INFINITY",
